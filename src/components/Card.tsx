d34e687a18a58b1f17aec2ffa9f1cd957ec04aa6
Annie Huang <annie.hn@gmail.com>
2024-04-20 22:00:15 +1000

    Pass 'bg' prop into Card component with a default value.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,12 @@
 import { FC, PropsWithChildren } from 'react';
 
-export interface CardProps {}
+export interface CardProps {
+  bg?: string;
+}
 
-export const Card: FC<PropsWithChildren<CardProps>> = ({ children }) => {
-  return <div className='bg-gray-100 p-6 rounded-lg shadow-md'>{children}</div>;
+export const Card: FC<PropsWithChildren<CardProps>> = ({
+  children,
+  bg = 'bg-gray-100',
+}) => {
+  return <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>;
 };
