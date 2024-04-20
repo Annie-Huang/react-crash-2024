a36e147eb5297a15845b44d4f24e814abb2648ce
Annie Huang <annie.hn@gmail.com>
2024-04-20 21:35:38 +1000

    Get a default value for <Hero> props

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,14 @@
 import { FC } from 'react';
 
 export interface HeroProps {
-  title: string;
-  subtitle: string;
+  title?: string;
+  subtitle?: string;
 }
 
-export const Hero: FC<HeroProps> = ({ title, subtitle }) => {
+export const Hero: FC<HeroProps> = ({
+  title = 'Become a React Dev',
+  subtitle = 'Find the React job that fits your skills and needs',
+}) => {
   return (
     <section className='bg-indigo-700 py-20 mb-4'>
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center'>
