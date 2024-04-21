c466bf2817ac78cb485c7689a4de45b213984ff4
Annie Huang <annie.hn@gmail.com>
2024-04-21 16:58:49 +1000

    Add useEffect to fetch for a job detail in <JobPage>

--- src/pages/JobPage.tsx
+++ src/pages/JobPage.tsx
@@ -1,3 +1,28 @@
+import { useEffect, useState } from 'react';
+import { Job } from '../components/JobListing.tsx';
+import { useParams } from 'react-router-dom';
+
 export const JobPage = () => {
+  const { id } = useParams();
+  const [job, setJob] = useState<Job | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchJob = async () => {
+      try {
+        const response = await fetch(`/api/jobs/${id}`);
+        const data = await response.json();
+        console.log(data);
+        setJob(data);
+      } catch (error) {
+        console.error('Error fetching data', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJob();
+  }, []);
+
   return <div>JobPage</div>;
 };
