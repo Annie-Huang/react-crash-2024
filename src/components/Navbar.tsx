7b5b26c7ed85590aab9be562f0dfce904436781f
Annie Huang <annie.hn@gmail.com>
2024-04-21 12:53:37 +1000

    Switch <a> to use <Link> in <Navbar>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import logo from '../assets/images/logo.png';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
@@ -7,36 +8,37 @@ export const Navbar = () => {
         <div className='flex h-20 items-center justify-between'>
           <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
             {/* <!-- Logo --> */}
+            {/* <a> will do a whole page refresh
             <a
               className='flex flex-shrink-0 items-center mr-4'
               href='/index.html'
-            >
+            >*/}
+            <Link className='flex flex-shrink-0 items-center mr-4' to='/'>
               <img className='h-10 w-auto' src={logo} alt='React Jobs' />
               <span className='hidden md:block text-white text-2xl font-bold ml-2'>
                 React Jobs
               </span>
-            </a>
+            </Link>
             <div className='md:ml-auto'>
               <div className='flex space-x-2'>
-                {/* <a> will do a whole page refresh */}
-                <a
-                  href='/'
+                <Link
+                  to='/'
                   className='text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
                 >
                   Home
-                </a>
-                <a
-                  href='/jobs'
+                </Link>
+                <Link
+                  to='/jobs'
                   className='text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
                 >
                   Jobs
-                </a>
-                <a
-                  href='/add-job'
+                </Link>
+                <Link
+                  to='/add-job'
                   className='text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
                 >
                   Add Job
-                </a>
+                </Link>
               </div>
             </div>
           </div>
