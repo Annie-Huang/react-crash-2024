f6a79393c6467d4adfcd6c2f41cd3ed3a5289b8f
Annie Huang <annie.hn@gmail.com>
2024-04-22 16:37:00 +1000

    Add <ToastifyContainer /> into the <MainLayout>

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import { Navbar } from '../components/Navbar.tsx';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 export const MainLayout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      {/* You can put the ToastifyContainer anywhere as it will absolute position it.*/}
+      <ToastContainer />
     </>
   );
 };
